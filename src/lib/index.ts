bbbf09d461b107d8f740361f1a299e97b496c059
Dariusz Filipiak <dariusz.filipiak@igt.com>
2023-05-01 03:26:34 +0200

    update

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,21 +1,20 @@
-/*
- * Public API Surface of ngx-store
- */
 
-// Public classes.
-export {
-  CookieStorage, LocalStorage, SessionStorage, SharedStorage, SharedStorage as TempStorage,
-} from './decorator/webstorage';
-export { WebStorageService } from './service/webstorage.service';
-export {
-  CookiesStorageService, CookiesStorageService as CookieStorageService,
-} from './service/cookies-storage.service';
-export { SharedStorageService, SharedStorageService as TempStorageService } from './service/shared-storage.service';
-export { LocalStorageService } from './service/local-storage.service';
-export { SessionStorageService } from './service/session-storage.service';
-export { WebStorageConfigInterface } from './config/config.interface';
-export { Webstorable, WebstorableArray, WebstorableObject } from './ngx-store.types';
-export { NgxStorageEvent } from './utility/storage/storage-event';
-export { Resource as NgxResource } from './service/resource';
-export { NgxStoreModule, NgxStoreModule as WebStorageModule } from './ngx-store.module';
-export * from './ngx-store.types';
+import "reflect-metadata";
+
+import * as localForge from 'localforage';
+
+const stor = localForge.createInstance({
+  driver: localForge.INDEXEDDB,
+  storeName: 'app-firedev-admin-mode-configuration'
+})
+
+const formatMetadataKey = Symbol("format");
+
+export function Cache(formatString?: string) {
+  return Reflect.metadata(formatMetadataKey, formatString);
+}
+
+
+// export function getFormat(target: any, propertyKey: string) {
+//   return Reflect.getMetadata(formatMetadataKey, target, propertyKey);
+// }
