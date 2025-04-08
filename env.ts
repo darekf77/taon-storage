6ff903cb740da6f25bc299177d67c3953ebe82d7
Dariusz Filipiak <darekf77@gmail.com>
2025-04-08 20:28:55 +0200

    release: v18.0.30

---
+++ env.ts
@@ -0,0 +1,6 @@
+import type { EnvOptions } from 'tnp/src';
+
+const env: Partial<EnvOptions> = {
+  website: { domain: 'taon-storage.example.domain.com', title: 'Taon Storage' },
+};
+export default env;
